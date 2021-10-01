dbd4ab2eb50a62c978d61f3dec7efce6af9cf4df
cag <poorcag@protonmail.com>
2021-10-01 23:34:06 +1000

    Added bot and requirements.txt files

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "bot" in message.content:
+        response = "<:feelshekman:588164589238091802>"
+        await message.channel.send(response)
+
+client.run(TOKEN)
